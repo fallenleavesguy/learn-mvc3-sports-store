e48b4a0666c58ea893abbcba06511da8700aa4d1
donghs <xx@xx.com>
2024-10-05 00:43:19 +0800

    feat: add mock Product List page

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -25,7 +25,7 @@ namespace SportsStore.WebUI
             routes.MapRoute(
                 "Default", // 路由名称
                 "{controller}/{action}/{id}", // 带有参数的 URL
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // 参数默认值
+                new { controller = "Product", action = "List", id = UrlParameter.Optional } // 参数默认值
             );
 
         }
