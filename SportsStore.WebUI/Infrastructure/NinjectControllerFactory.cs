eef9f9c87215ef2518626bcb5cbf2dd859cd94eb
donghs <xx@xx.com>
2024-10-04 22:25:09 +0800

    add product model

--- SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
+++ SportsStore.WebUI/Infrastructure/NinjectControllerFactory.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿//using System;
+//using System.Web.Mvc;
+//using System.Web.Routing;
+//using Ninject;
+//using SportsStore.Domain.Abstract;
+//using System.Collections.Generic;
+//using SportsStore.Domain.Entities;
+//using System.Linq;
+//using Moq;
+using System;
+using System.Configuration;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Ninject;
+using SportsStore.Domain.Abstract;
+using Moq;
+using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+using System.Linq;
 
 namespace SportsStore.WebUI.Infrastructure
 {
-    public class NinjectControllerFactory: DefaultControllerFactory
+    public class NinjectControllerFactory : DefaultControllerFactory
     {
         private IKernel ninjectKernel;
 
@@ -24,6 +39,15 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBindings()
         {
+            // Mock implementation of the IProductRepository Interface
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products)
+                .Returns(new List<Product> {
+                    new Product { Name = "Football", Price = 25 },
+                    new Product { Name = "Surf board", Price = 179 },
+                    new Product { Name = "Running shoes", Price = 95 }
+                }.AsQueryable());
+            ninjectKernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
